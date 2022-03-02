d9f3f31402c14ab3bd5c817f7ab99025a7782760
Samuel-3000 <95452056+Samuel-3000@users.noreply.github.com>
2022-03-01 21:19:19 -0500

    commit

---
+++ arithmetic calculation 2/arithmeticModule.js
@@ -0,0 +1,29 @@
+const add = function(value_1,value_2){
+    console.log("---------------------------------🄰🄳🄳")
+    console.log(`Value 1 is --------->${value_1}`)
+    console.log(`Value 2 is --------->${value_2}`)
+    console.log(`Result is therefore ${value_1 + value_2} :)`)
+};
+
+const subtract = function(value_1,value_2){
+    console.log("--------------------------🅂🅄🄱🅃🅁🄰🄲🅃")
+    console.log(`Value 1 is --------->${value_1}`)
+    console.log(`Value 2 is --------->${value_2}`)
+    console.log(`Result is therefore ${value_1 - value_2} :)`)
+};
+
+const multiply = function(value_1,value_2){
+    console.log("-------------------------🄼🅄🄻🅃🄸🄿🄻🅈")
+    console.log(`Value 1 is --------->${value_1}`)
+    console.log(`Value 2 is --------->${value_2}`)
+    console.log(`Result is therefore ${value_1 * value_2} :)`)
+};
+
+const divide = function(value_1,value_2){
+    console.log("------------------------------🄳🄸🅅🄸🄳🄴")
+    console.log(`Value 1 is --------->${value_1}`)
+    console.log(`Value 2 is --------->${value_2}`)
+    console.log(`Result is therefore ${value_1 / value_2} :)`)
+};
+
+module.exports = {add, subtract, multiply, divide}
